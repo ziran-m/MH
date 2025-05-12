cccb24e62395941558a94cff9724820f98623c80
ziran <aluuuly@163.com>
2025-05-12 10:20:51 +0800

    version1.2

---
+++ module/wabao.py
@@ -0,0 +1,24 @@
+import threading
+
+from mh_script.client_manager.launcher import Launcher
+from mh_script.handler.wabao_handler import WaBao
+
+def run_in_thread(task, idx):
+    thread = threading.Thread(target=task.do, args=(idx,), daemon=True)
+    thread.start()
+
+
+def main():
+    launcher = Launcher()
+    regions = launcher.get_regions()
+    if not regions:
+        return
+
+    launcher.resize_and_move_window()
+
+    task = WaBao(regions)
+    run_in_thread(task, 0)  # 使用子线程执行任务，避免主线程卡死
+
+
+if __name__ == "__main__":
+    main()
