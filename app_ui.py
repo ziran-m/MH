c75b217a6e268916e19f9f2fca63306bcf0c80f4
ziran <aluuuly@163.com>
2025-05-14 18:12:49 +0800

    version1.2

--- app_ui.py
+++ app_ui.py
@@ -10,17 +10,32 @@ from mh_script.utils.log_util import TextHandler, logger, global_log
 
 class App:
     def __init__(self):
-        ctk.set_appearance_mode("light")
+
+
+
+        ctk.set_appearance_mode("dark")
         ctk.set_default_color_theme("blue")
         self.root = ctk.CTk()
-        self.root.geometry("700x500")
+        self.root.geometry("700x400")
         self.root.title("脚本启动器[当前只支持2k显示器]")
 
         self.font_style = ("Microsoft YaHei", 14)
+        self.button_config = {
+            "font": self.font_style,
+            "height": 30,
+            "width": 100,
+            "corner_radius": 10,
+            "fg_color": "transparent",  # 与背景一致
+            "hover_color": "#3a3a3a",  # 灰色，hover 时显现
+            "text_color": "white",  # 文字保持白色可读
+            "bg_color": "transparent"  # 避免覆盖父容器背景
+        }
 
         self.setup_layout()
         self.create_buttons()
         self.create_log_ui()
+
+        self.create_config_ui()
 
         self.root.mainloop()
 
@@ -36,76 +51,49 @@ class App:
         # 让日志区随窗口伸缩
         self.root.grid_rowconfigure(0, weight=1)
         self.root.grid_columnconfigure(1, weight=1)
-
+        # 配置页面
+        self.config_frame = ctk.CTkFrame(self.root)
+        self.config_frame.grid(row=0, column=0, padx=10, pady=10, sticky="ns")
+        self.config_frame.grid_remove()  # 默认隐藏
+    # 主页面
     def create_buttons(self):
-        button_config = {
-            "font": self.font_style,
-            "height": 40,
-            "width": 100,
-            "corner_radius": 10,
-            "fg_color": "#1abc9c",
-            "hover_color": "#2A6E84",
-            "text_color": "white"
-
-        }
-        # 创建一个复制的字典，并覆盖颜色配置
-        exit_button_config = button_config.copy()
-        exit_button_config.update({
-            "fg_color": "#e74c3c",
-            "hover_color": "#660000",
-            "text_color": "white"
-        })
 
         row = 0
-        self.open_button = ctk.CTkButton(self.left_frame, text="启动", command=self.start_task, **button_config)
+        self.open_button = ctk.CTkButton(self.left_frame, text="启动", command=self.start_task, **self.button_config)
         self.open_button.grid(row=row, column=0, columnspan=2,padx=10, pady=(0, 10), sticky="ew")
 
         row += 1
         self.dungeon_task_button = ctk.CTkButton(self.left_frame, text="副本", command=self.dungeon_task_task,
-                                                 **button_config)
-        self.dungeon_task_button.grid(row=row, column=0,padx=10, pady=(0, 10), sticky="ew")
-
-        # 副本输入框放在副本按钮旁边
-        self.dungeon_num_entry = ctk.CTkEntry(self.left_frame, placeholder_text="副本开始点 0 侠士 1 普本1 2 普本2",
-                                              font=self.font_style, width=150, height=40,border_color="#95a5a6", border_width=2)
-        self.dungeon_num_entry.grid(row=row, column=1, padx=10, pady=(0, 10), sticky="ew")
-
-
+                                                 **self.button_config)
+        self.dungeon_task_button.grid(row=row, column=0, columnspan=2,padx=10, pady=(0, 10), sticky="ew")
         row += 1
         # 日常按钮跨越两列
-        self.daily_button = ctk.CTkButton(self.left_frame, text="日常", command=self.daily_task, **button_config)
+        self.daily_button = ctk.CTkButton(self.left_frame, text="日常", command=self.daily_task, **self.button_config)
         self.daily_button.grid(row=row, column=0, columnspan=2,padx=10, pady=(0, 10), sticky="ew")
 
         row += 1
-        self.ghost_button = ctk.CTkButton(self.left_frame, text="抓鬼", command=self.ghost_task, **button_config)
-        self.ghost_button.grid(row=row, column=0,padx=10, pady=(0, 10), sticky="ew")
+        self.ghost_button = ctk.CTkButton(self.left_frame, text="抓鬼", command=self.ghost_task, **self.button_config)
+        self.ghost_button.grid(row=row, column=0, columnspan=2,padx=10, pady=(0, 10), sticky="ew")
 
-        # 抓鬼轮数输入框放在抓鬼按钮旁边
-        self.ghost_num_entry = ctk.CTkEntry(self.left_frame, placeholder_text="抓鬼轮数 (默认2)", font=self.font_style,
-                                            width=150, height=40,border_color="#95a5a6", border_width=2)
-        self.ghost_num_entry.grid(row=row, column=1, padx=10, pady=(0, 10), sticky="ew")
 
         row += 1
-        self.button_320 = ctk.CTkButton(self.left_frame, text="320", command=self.task_320, **button_config)
+        self.button_320 = ctk.CTkButton(self.left_frame, text="320", command=self.task_320, **self.button_config)
         self.button_320.grid(row=row, column=0, columnspan=2,padx=10, pady=(0, 10), sticky="ew")
 
         row += 1
-        self.wabao_button = ctk.CTkButton(self.left_frame, text="考古", command=self.wabao_task, **button_config)
+        self.wabao_button = ctk.CTkButton(self.left_frame, text="考古", command=self.wabao_task, **self.button_config)
         self.wabao_button.grid(row=row, column=0, columnspan=2,padx=10, pady=(0, 10), sticky="ew")
+
+
         row += 1
-        self.path_entry = ctk.CTkEntry(self.left_frame, placeholder_text="文件运行地址",
-                                              font=self.font_style, width=150, height=40, border_color="#95a5a6",
-                                              border_width=2)
-        self.path_entry.grid(row=row, column=0,columnspan=2, padx=10, pady=(0, 10), sticky="ew")
+        self.config_button = ctk.CTkButton(self.left_frame, text="配置", command=self.show_config_ui, **self.button_config)
+        self.config_button.grid(row=row, column=0, columnspan=2, padx=10, pady=(0, 10), sticky="ew")
 
         # 添加退出按钮到 left_frame（假设 row 是从上往下排的）
-        self.exit_button = ctk.CTkButton(self.left_frame, text="退出", command=self.root.quit,**exit_button_config)
+        self.exit_button = ctk.CTkButton(self.left_frame, text="退出", command=self.root.quit,**self.button_config)
         self.exit_button.grid(row=99, column=0, columnspan=2,padx=10, pady=(0, 10), sticky="ew")
 
-        # 使按钮和输入框宽度一致
-        self.left_frame.grid_columnconfigure(0, weight=1)  # 使按钮的列宽度可伸缩
-        self.left_frame.grid_columnconfigure(1, weight=1)  # 使输入框的列宽度可伸缩
-
+    # 日志页面
     def create_log_ui(self):
         """创建日志区域（文本框和滚动条）"""
 
@@ -128,7 +116,50 @@ class App:
         # 设置日志 handler
         self.log_text_handler = TextHandler(self.log_text)
         logger.addHandler(self.log_text_handler)
+    # 配置页面
+    def create_config_ui(self):
 
+        """创建配置界面但默认隐藏"""
+
+        row = 0
+        self.dungeon_task_button = ctk.CTkButton(self.config_frame, text="副本", command=self.dungeon_task_task,
+                                                 **self.button_config)
+        self.dungeon_task_button.grid(row=row, column=0, padx=10, pady=(0, 10), sticky="ew")
+
+        # 副本输入框放在副本按钮旁边
+        self.dungeon_num_entry = ctk.CTkEntry(self.config_frame, placeholder_text="副本开始点 0 侠士 1 普本1 2 普本2",
+                                              font=self.font_style, width=150, height=40, border_color="#95a5a6",
+                                              border_width=2)
+        self.dungeon_num_entry.grid(row=row, column=1, padx=10, pady=(0, 10), sticky="ew")
+
+
+        row += 1
+        self.ghost_button = ctk.CTkButton(self.config_frame, text="抓鬼", command=self.ghost_task, **self.button_config)
+        self.ghost_button.grid(row=row, column=0, padx=10, pady=(0, 10), sticky="ew")
+
+        # 抓鬼轮数输入框放在抓鬼按钮旁边
+        self.ghost_num_entry = ctk.CTkEntry(self.config_frame, placeholder_text="抓鬼轮数 (默认2)", font=self.font_style,
+                                            width=150, height=40, border_color="#95a5a6", border_width=2)
+        self.ghost_num_entry.grid(row=row, column=1, padx=10, pady=(0, 10), sticky="ew")
+        row += 1
+        self.path_entry = ctk.CTkEntry(self.config_frame, placeholder_text="文件运行地址",
+                                       font=self.font_style, width=150, height=40, border_color="#95a5a6",
+                                       border_width=2)
+        self.path_entry.grid(row=row, column=0, columnspan=2, padx=10, pady=(0, 10), sticky="ew")
+        # 添加退出按钮到 left_frame（假设 row 是从上往下排的）
+        self.back_button = ctk.CTkButton(self.config_frame, text="返回", command=self.show_main_ui, **self.button_config)
+        self.back_button.grid(row=99, column=0, columnspan=2, padx=10, pady=(0, 10), sticky="ew")
+
+
+    def show_config_ui(self):
+        """显示配置界面，隐藏主按钮区域"""
+        self.left_frame.grid_remove()
+        self.config_frame.grid()
+
+    def show_main_ui(self):
+        """返回主按钮区域，隐藏配置界面"""
+        self.config_frame.grid_remove()
+        self.left_frame.grid()
     def execute_file(self, file_name):
         """加载并执行指定文件的 main 方法"""
         try:
@@ -222,6 +253,7 @@ class App:
         self.ghost_button.configure(state="disabled")
         self.wabao_button.configure(state="disabled")
         self.path_entry.configure(state="disabled")
+        self.config_button.configure(state="disabled")
 
         # 设置1秒后重新启用按钮
         self.root.after(1000, self.enable_buttons)
@@ -235,6 +267,7 @@ class App:
         self.ghost_button.configure(state="normal")
         self.wabao_button.configure(state="normal")
         self.path_entry.configure(state="normal")
+        self.config_button.configure(state="normal")
 
 
 if __name__ == "__main__":
