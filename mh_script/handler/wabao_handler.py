133ca31018936b651c17cb5adc5a78af43d6f450
ziran <aluuuly@163.com>
2025-05-23 17:37:30 +0800

    version1.2

--- mh_script/handler/wabao_handler.py
+++ mh_script/handler/wabao_handler.py
@@ -11,48 +11,49 @@ class WaBao:
         self.basicHandler = BasicHandler(ocrPlayer)
 
     def do(self, region: ScreenRegion = None):
-        self.basicHandler.clean(region)
+        while True:
+            self.basicHandler.clean(region)
 
-        pos = self.ocrPlayer.find_by_pic_first(region, "common.bag", 0.7)
-        if pos is not None:
-            log.info(f"[考古] 点击包裹图标：{pos}")
-            self.ocrPlayer.touch(pos, True, None)
-            self.delay()
-
-        pos = self.ocrPlayer.find_by_pic_first(region, "common.clean_up")
-        if pos is not None:
-            log.info(f"[考古] 点击整理：{pos}")
-            self.ocrPlayer.touch(pos, True, None)
-            self.delay()
-
-        pos = self.basicHandler.smart_find_bag_pic_with_scroll(region, "wabao.wabao", None, 0.8, False,
-                                                           self.basicHandler.get_center(region))
-        if pos is None:
-            log.info("[考古] 找不到铲子，考古流程结束")
-            return
-        self.ocrPlayer.doubleTouch(pos, True, None)
-        self.delay()
-
-        pos = self.ocrPlayer.find_by_pic_first(region, "wabao.kaogu")
-        if pos is None:
-            log.info("[考古] 找不到考古，考古流程结束")
-            return
-        self.ocrPlayer.touch(pos, True, None)
-        self.delay()
-
-        dig_flag = True
-        times = 0
-        while dig_flag:
-            pos = self.ocrPlayer.find_by_pic_first(region, "wabao.wajue")
+            pos = self.ocrPlayer.find_by_pic_first(region, "common.bag", 0.7)
             if pos is not None:
-                log.info(f"[考古] 点击使用：{pos}")
-                self.ocrPlayer.touch(pos, False, None)
-                times = 0
-            times += 1
-            self.delay(2, 2)
-            if times % 40 == 0:
-                log.info("[考古] 超过80秒未发现考古使用按钮，结束考古")
-                dig_flag = False
+                log.info(f"[考古] 点击包裹图标：{pos}")
+                self.ocrPlayer.touch(pos, True, None)
+                self.delay()
+
+            pos = self.ocrPlayer.find_by_pic_first(region, "common.clean_up")
+            if pos is not None:
+                log.info(f"[考古] 点击整理：{pos}")
+                self.ocrPlayer.touch(pos, True, None)
+                self.delay()
+
+            pos = self.basicHandler.smart_find_bag_pic_with_scroll(region, "wabao.wabao", None, 0.8, False,
+                                                               self.basicHandler.get_center(region))
+            if pos is None:
+                log.info("[考古] 找不到铲子，考古流程结束")
+                break
+            self.ocrPlayer.doubleTouch(pos, True, None)
+            self.delay()
+
+            pos = self.ocrPlayer.find_by_pic_first(region, "wabao.kaogu")
+            if pos is None:
+                log.info("[考古] 找不到考古，考古流程结束")
+                break
+            self.ocrPlayer.touch(pos, True, None)
+            self.delay()
+
+            dig_flag = True
+            times = 0
+            while dig_flag:
+                pos = self.ocrPlayer.find_by_pic_first(region, "wabao.wajue")
+                if pos is not None:
+                    log.info(f"[考古] 点击使用：{pos}")
+                    self.ocrPlayer.touch(pos, False, None)
+                    times = 0
+                times += 1
+                self.delay(2, 2)
+                if times % 40 == 0:
+                    log.info("[考古] 超过80秒未发现考古使用按钮，结束考古")
+                    dig_flag = False
 
         log.info("[考古] 考古完成")
 
